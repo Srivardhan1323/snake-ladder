618615f790256c93a57b5fd89434f0c16c7d4ad8
Srivardhan_1323 <srivardhan984@gmail.com>
2024-03-20 16:53:37 +0530

    Added some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Colourbox from './Colourbox.jsx'
 import Colourboxgrid from './Colourboxgrid.jsx'
 import Player from './Player.jsx'
 function App() {
-  
   return (
      <div>
        <Player /> 
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,7 @@
 import Colourboxgrid from "./Colourboxgrid";
 import { useState } from "react";
 import "./Player.css"
+import React from 'react';
 const snake=[
     {start:17,end:7},
     {start:54,end:34},
@@ -22,67 +23,79 @@ const snake=[
     {start:80,end:100}
   ]
 export default function Player(){
+    const [chance,changechance]=useState(1);
     const [p1,changep1]=useState(0);
     const [p2,changep2]=useState(0);
     const [die,rolldie]=useState(0);
+    const Chance =()=>{
+      alert("Chance is not yours");
+    }
+    const snk=()=>{
+      alert("Snake Bited");
+    }
+    const ldr =()=>{
+      changechance(!chance);
+      alert("Ladder! One more chance hurray!!");
+    }
     const rollDiep1=() =>
     {
       let temp=Math.floor(Math.random()*6)+1;
       changep1(Math.min(temp+p1,100));
       rolldie(temp);
+      changechance(!chance);
     }
     const rollDiep2=() =>
     {
       let temp=Math.floor(Math.random()*6)+1;
       changep2(Math.min(temp+p2,100));
       rolldie(temp);
+      changechance(!chance);
     }
     for(let i=0;i<snake.length;i++){
         if(snake[i].start===p1)
         {
-          alert("Snakep1");
           changep1(snake[i].end);
+          snk();
           break;
         }
       }
     for(let i=0;i<snake.length;i++){
         if(snake[i].start===p2)
         {
-          alert("Snakep2");
           changep2(snake[i].end);
+          snk();
           break;
         }
       }
     for(let i=0;i<ladder.length;i++){
         if(ladder[i].start===p1)
         {
-          alert("LADDER");
           changep1(ladder[i].end);
+          ldr();
           break;
         }
       }
     for(let i=0;i<ladder.length;i++){
         if(ladder[i].start===p2)
         {
-          alert("LADDER");
           changep2(ladder[i].end);
+          ldr();
           break;
         }
       }
     return(
         <div>
-            <h1>Dievalue:{die}</h1>
+            <h2>Dievalue:{die}</h2>
             <div className="board">
-                <div className="player">
-                    <h2>Player:p1</h2>
-                    <button onClick={rollDiep1}>Roll die</button>
+                <div className="player" temp1 >
+                    <h2>Player:Srivardhan</h2>
+                    {chance?<button onClick={rollDiep1}>Roll die</button>:<button onClick={Chance} className="disable">Roll die</button>}
                     <h5>Position:{p1}</h5>
                 </div>
                      <Colourboxgrid p1={p1} p2={p2} Colour1="skyblue" Colour2="yellow"/>
-                <div className="player">
-                    
-                    <h2>Player:p2</h2>
-                    <button onClick={rollDiep2}>Roll die</button>
+                <div className="player" >
+                    <h2>Player:Rithwik</h2>
+                    {!chance?<button onClick={rollDiep2}>Roll die</button>:<button onClick={Chance} className="disable">Roll die</button>}
                     <h5>Position:{p2}</h5>
                 </div>
             </div>
